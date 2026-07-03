ec90f1a8b1f97c8edb1cd1998a2aed3d708826d4
Melodicrum <196373088+GideonTul@users.noreply.github.com>
2026-07-03 13:38:13 -0600

    Some monster setup

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,12 +9,18 @@ public class EnemyAI : MonoBehaviour
     private NavMeshAgent agent;
     public Animator anim;
 
+    public AudioClip aggroSound;
+
+    private GameObject player;
+
+    private bool isMad = false;
+
     private int currentPoint = 0;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-
+        player = GameObject.FindGameObjectWithTag("Player");
 
         if (patrolPoints.Length > 0)
         {
@@ -25,6 +32,9 @@ public class EnemyAI : MonoBehaviour
     {
         if (patrolPoints.Length == 0)
             return;
+
+        float distFromPlayer = Vector3.Distance(transform.position, player.transform.position);
+        ControlAggro(distFromPlayer);
 
         // Update animation
         anim.SetFloat("Speed", agent.velocity.magnitude);
@@ -39,6 +49,20 @@ public class EnemyAI : MonoBehaviour
                 currentPoint = 0;
 
             agent.destination = patrolPoints[currentPoint].position;
+        }
+    }
+
+    private void ControlAggro(float dist)
+    {
+        if (isMad) return;
+
+        if (dist < 20) { 
+            isMad = true;
+            AudioManager.Instance.PlaySFX(aggroSound, 0.5f);
+        }
+        else
+        {
+            isMad = false;
         }
     }
 }
